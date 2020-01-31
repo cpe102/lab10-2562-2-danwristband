4890b7df0c0c5bf7ed82b7541668088aa6241127
danwristband <59277358+danwristband@users.noreply.github.com>
2020-01-31 16:45:21 +0700

    first

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,2 +1,59 @@
 // cout << "Press Enter 3 times to reveal your future.";
 // cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+using namespace std;
+int x;
+string Ramdom(int b)
+{
+    
+    switch (b)
+    {
+    case 0:
+        cout << "A";
+        break;
+    case 1:
+        cout << "B";
+        break;
+    case 2:
+        cout << "B+";
+        break;
+    case 3:
+        cout << "C";
+        break;
+    case 4:
+        cout << "C+";
+        break;
+    case 5:
+        cout << "D";
+        break;
+    case 6:
+        cout << "D+";
+        break;
+    case 7:
+        cout << "F";
+        break;
+    case 8:
+        cout << "W";
+        break;
+        
+    }
+    
+}
+int main()
+{
+    string a;
+    cout << "Press Enter 3 times to reveal your future."<<" ";
+    getline(cin, a);
+    getline(cin, a);
+    getline(cin, a);
+    srand(time(0));
+    x = rand() % 9;
+    cout << "You will get"<<" ";
+    Ramdom(x);
+    cout<<" " << "in this 261102.";
+    return 0;
+}
+
